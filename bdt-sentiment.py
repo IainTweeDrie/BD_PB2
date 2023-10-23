e4c51f679e34d4e9a759b83332f065553375ce9e
Twee180 <iain@unite180.com>
2023-10-23 23:21:36 +0200

    changed scripts and added env var to dockerfile

--- bdt-sentiment.py
+++ bdt-sentiment.py
@@ -1,39 +1,46 @@
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import os
+from transformers import pipeline
 from google.cloud import storage
+import csv
 
-def fetch_tweets_from_gcs(bucket_name, file_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    blob = storage.Blob(file_name, bucket)
-    content = blob.download_as_text()
-    tweets = content.splitlines()
-    return tweets
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
 
 
 def main():
-    BUCKET_NAME = 'GCS-BUCKET-NAME'
-    FILE_NAME = 'STORED-TWEETS-FILE-NAME'
+    # Step 1: Download the source file from Google Cloud Storage
+    BUCKET_NAME = 'tweets'
+    SOURCE_BLOB_NAME = 'source_file.csv'
+    DESTINATION_FILE_NAME = 'local_tweets.csv'
+    download_blob(BUCKET_NAME, SOURCE_BLOB_NAME, DESTINATION_FILE_NAME)
 
-    tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-    model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-    sentiment_analysis_pipeline = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-
-    tweets_with_ground_truth = fetch_tweets_from_gcs(BUCKET_NAME, FILE_NAME)
+    # Step 2: Use Hugging Face pre-trained transformer to get sentiments
+    nlp = pipeline("sentiment-analysis")
 
     correct_predictions = 0
+    total_predictions = 0
 
-    for tweet_with_ground_truth in tweets_with_ground_truth:
-        tweet, ground_truth = tweet_with_ground_truth.rsplit(',', 1)
-        ground_truth = ground_truth.strip()
+    # Export to CSV
+    with open(DESTINATION_FILE_NAME, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+        for row in reader:
+            tweet, ground_truth = row
+            result = nlp(tweet)
+            predicted_sentiment = result[0]['label']
+            if predicted_sentiment == ground_truth:
+                correct_predictions += 1
+            total_predictions += 1
 
-        result = sentiment_analysis_pipeline(tweet)[0]
-        predicted_sentiment = result['label'].lower()
-
-        if predicted_sentiment == ground_truth:
-            correct_predictions += 1
-
-    accuracy = correct_predictions / len(tweets_with_ground_truth)
-    print(f'Accuracy of the pre-trained model: {accuracy * 100:.2f}%')
+    # Step 3: Calculate accuracy
+    accuracy = (correct_predictions / total_predictions) * 100
+    print(f"Accuracy: {accuracy:.2f}%")
 
 
 if __name__ == "__main__":
